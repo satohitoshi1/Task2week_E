bc7ba8f91ca030acb8a53b1b76be296ad7ea717d
satohitoshi1 <satohitoshi121@gmail.com>
2022-06-27 21:36:04 +0900

    sard commit

--- 課題E.py
+++ 課題E.py
@@ -2,32 +2,25 @@ import requests
 import time
 
 # 使わなそう ❍❍ = {"X-Api-Key": "634d2f44705342cd9008bf457693044b"}
-# API使うと記事番号で取得できる？(BS4いらなくなる罠まじでスパルタワロタ)
+# BS4いらなくなる罠
 
 
 def get_title_link_url():
     base_url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
     res = requests.get(base_url)
-    dic = res.json()  # 記事番号を辞書化
-
+    dic = res.json()  # 番号を辞書化
     numbers = []
-    for number in range(0, 30):
-        numbers.append(dic[number])   # 記事番号を30回取得
-
+    for number in range(0, 50):  # 番号を50回取得
+        numbers.append(dic[number])
     return numbers
 
 
-def get_info(number):
-
-    for r in number:
-        article_number = r
-        response = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{article_number}.json?print=pretty"
+def get_info(numbers):
+    for n in numbers:
+        res = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{n}.json?print=pretty"
         )
-
-        dic = response.json()  # パース
-        # print(dic)
-
+        dic = res.json()
         title = dic["title"]
 
         if "url" in dic:
